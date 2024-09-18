219b9c7c86407bc127ac949d71af304e92d3a2b9
beofme31 <beome31@naver.com>
2024-09-19 00:15:10 +0900

    변수와 데이터 타입

--- udemy_jonas/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ udemy_jonas/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,45 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
-console.log(23);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Jonas';
+// let firstName = 'Matilda';
+
+// 변수는 '값을 저장할 수 있는 박스', 현실에서 상자 안에 내용물을 담고 라벨을 붙여서 라벨을 보고 찾는 원리와 같이 작동한다.
+// 변수를 통해 여러번 선언이 가능하다.
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+//  let 3years = 3; // Invalid or unexpected token 숫자로 시작할 수 없다.
+
+// let jonas& matilda = 'JM'; // Unexpected token '&'
+// let new = 27; // Unexpected token 'new' 예약어도 사용할 수 없다.
+// let $function; // function은 예약어지만 앞에 $를 붙이면 사용 가능
+// let person = 'Jonas' // 변수 명명할 때는 소문자로 시작하는 명사로 한다.
+// let PI = 3.1415; // 절대 바뀌지 않을 데이터는 상수로 할 때 대문자로 해준다.
+
+// let myFirstJob = 'Coder';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob)
+
+//   The 7 primitive data types
+//   1. number: Used for demicals and integers
+//   2. String: Used for text
+//   3. Boolean: Used for taking decisions (ex. true/false)
+//   4. Undefined: Value taken by a variable that is not yet defined('empty value')
+//   5. Null: Also means 'empty value'
+//   6. Symbol(ES2015): Value that is unique and cannot be changed [Not useful for now]
+//   7. Bigint(ES2020): Larger integers than the Number type can hold
+
+//  JavaScript has dynamic typing: We don't have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically.
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
