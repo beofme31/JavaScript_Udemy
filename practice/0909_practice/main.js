97448ae24605859ec1cc8acad764b163a53470a1
beofme31 <beome31@naver.com>
2024-09-18 23:44:37 +0900

    "기존 연습 코드 삭제"

--- practice/0909_practice/main.js
+++
@@ -1,7 +0,0 @@
-myLoop: for(let no1 = 0; no1 < 2; no1++){
-  console.log(`position 1: ${no1}`)
-  for(let no2 = 0; no2 < 2; no2++){
-    console.log(`position 2: ${no1}, ${no2}`);
-    break myLoop
-  }
-}
