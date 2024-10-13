2358d31470fc2071f6d7c6689feef7bc3b15b3e2
beofme31 <beofme31@naver.com>
2024-10-13 22:50:54 +0900

    Functions accepting Callback functions

--- udemy_jonas/complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
+++ udemy_jonas/complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,33 +1,63 @@
 'use strict';
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 24739479284
-}
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284
+// }
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 24739479284) {
-    alert('Checked in')
-  } else {
-    alert('Wrong passport!')
-  }
-}
+//   if (passenger.passport === 24739479284) {
+//     alert('Checked in')
+//   } else {
+//     alert('Wrong passport!')
+//   }
+// }
 
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// }
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
 
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase()
+};
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000);
-}
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
 
-newPassport(jonas);
-checkIn(flight, jonas);
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('🖐️')
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
